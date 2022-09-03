14ec63d9267955c283a773b326b1ac7d690942ce
darkeclipz <larsrotgers@gmail.com>
2022-09-03 16:54:02 +0200

    lexical scoping

--- c1/square_roots.rkt
+++ c1/square_roots.rkt
@@ -1,24 +1,20 @@
 #lang racket
 
-(define (sqrt-iter guess x)
-    (new-if (good-enough? guess x)
-        guess
-        (sqrt-iter (improve guess x) x)))
-
-(define (improve guess x)
-    (average guess (/ x guess)))
-
-(define (average x y)
-    (/ (+ x y) 2))
-
-(define (good-enough? guess x)
-    (< (abs (- (square guess) x)) 0.001))
-
-(define (square x)
-    (* x x))
-
 (define (sqrt x)
-    (sqrt-iter 1.0 x))
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve guess))))
+    (define (improve guess)
+        (average guess (/ x guess)))
+    (define (average x y)
+        (/ (+ x y) 2))
+    (define (good-enough? guess)
+        (< (abs (- (square guess) x)) 0.001))
+    (define (square x)
+        (* x x))
+    (sqrt-iter 1.0))
+
 
 (define (new-if predicate
                 then-clause
@@ -51,3 +47,6 @@
 ; is known as normal-order evaluation, in contrast to 
 ; the “evaluate the arguments and then apply” method that the interpreter
 ; actually uses, which is called applicative-order evaluation.
+
+
+; lexical scoping
